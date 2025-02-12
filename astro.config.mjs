52e907c5ca014d0072a17dda0b7d9b87c0a0e2f4
Le Quang Thang <lethang7794@gmail.com>
2025-02-13 04:25:23 +0700

    config BASE_PATH

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+	integrations: [tailwind(), icon()],
+
+	// Deployments
+	site: import.meta.env.DOMAIN
+		? `https://${import.meta.env.DOMAIN}`
+		: undefined,
+	base: import.meta.env.BASE_URL,
 });
