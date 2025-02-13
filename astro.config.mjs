fe64d6c153847dbab52e028563648a37e1dcb10d
Le Quang Thang <lethang7794@gmail.com>
2025-02-13 10:45:56 +0700

    load env from env file, export github action var to workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import { loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), icon()],
 
 	// Deployments
-	site: process.env.PUBLIC_DOMAIN
-		? `https://${process.env.PUBLIC_DOMAIN}`
-		: undefined,
-	base: process.env.PUBLIC_BASE_URL || "",
+	site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
+	base: env.PUBLIC_BASE_URL || "",
 });
