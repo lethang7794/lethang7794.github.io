5af1eb0c9d1e24fc46d9e36f8de9f96f7e378a33
Lê Quang Thắng <lethang7794@gmail.com>
2025-02-15 23:34:33 +0700

    impr(pdf): print background and use page size from CSS (#5)
    * print background and use page size from CSS
    
    * use Inter variable font
    
    * use font-normal for text-skin-base
    
    * fix font statement, use color brand for certs name

--- e2e/generate-pdf.spec.ts
+++ e2e/generate-pdf.spec.ts
@@ -8,7 +8,12 @@ test.describe("New CV", () => {
 	test("should generate pdf from astro page", async ({ page }) => {
 		// Generates a PDF with 'print' media type.
 		await page.emulateMedia({ media: "print" });
-		const pdfBuffer = await page.pdf({ path: "playwright-result/CV.pdf", format: "A4" });
+		const pdfBuffer = await page.pdf({
+			path: "playwright-result/CV.pdf",
+			format: "A4",
+			printBackground: true,
+			preferCSSPageSize: true,
+		});
 
 		// Verify that the PDF was generated
 		expect(pdfBuffer.length).toBeGreaterThan(0);
