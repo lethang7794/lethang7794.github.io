2a84b5aceb03ab81ee87ff7912c2bdf65d72c304
Le Quang Thang <lethang7794@gmail.com>
2025-02-16 02:15:01 +0700

    fix: color-text-secondary variable name

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,7 +29,7 @@ export default {
       colors: {
         skin: {
           hue: withOpacity("--color-main"),
-          muted: withOpacity("--color-secondary"),
+          muted: withOpacity("--color-text-secondary"),
         },
       },
       ringColor: {
