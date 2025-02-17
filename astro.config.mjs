450822d7fee53bcb6d098922187dd1e096f35169
Le Quang Thang <lethang7794@gmail.com>
2025-02-18 00:39:57 +0700

    dep: react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { loadEnv } from "vite";
-
+import react from "@astrojs/react";
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon()],
-
-	// Deployments
-	site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
-	base: env.PUBLIC_BASE_URL || "",
+  integrations: [tailwind(), icon(), react()],
+  // Deployments
+  site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
+  base: env.PUBLIC_BASE_URL || ""
 });
