f7301f083505a3170d00487de828e33fdb488ad8
Lê Quang Thắng <lethang7794@gmail.com>
2025-02-19 18:11:13 +0700

    Revert "feat: add skills (#43)" (#45)
    This reverts commit 7a7085cb85a9ec80dfe995fa4ab01c9729fcc167.

--- src/constants/skill-icons.ts
+++ src/constants/skill-icons.ts
@@ -49,7 +49,6 @@ export const SKILLS_ICONS: Record<string, { iconName: string }> = {
 	Playwright: { iconName: "logos:playwright" },
 	"Protocol Buffers": { iconName: "protobuf" },
 	"React Hook Form": { iconName: "react-hook-form" },
-	"Radix UI": { iconName: "simple-icons:radixui" },
 	React: { iconName: "skill-icons:react-dark" },
 	Redux: { iconName: "skill-icons:redux" },
 	Regex: { iconName: "skill-icons:regex-dark" },
@@ -57,7 +56,6 @@ export const SKILLS_ICONS: Record<string, { iconName: string }> = {
 	SASS: { iconName: "skill-icons:sass" },
 	Selenium: { iconName: "skill-icons:selenium" },
 	Slack: { iconName: "logos:slack-icon" },
-	"shadcn/ui": { iconName: "simple-icons:shadcnui" },
 	"Styled Components": { iconName: "skill-icons:styledcomponents" },
 	"Tailwind CSS": { iconName: "skill-icons:tailwindcss-dark" },
 	Tailwind: { iconName: "skill-icons:tailwindcss-dark" },
@@ -65,5 +63,4 @@ export const SKILLS_ICONS: Record<string, { iconName: string }> = {
 	TypeScript: { iconName: "skill-icons:typescript" },
 	Vercel: { iconName: "skill-icons:vercel-dark" },
 	Windows: { iconName: "logos:microsoft-windows-icon" },
-	Zod: { iconName: "logos:zod" },
 };
