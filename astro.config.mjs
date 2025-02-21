a043a2adbc86d25a9c3698ff4da32abdb4697fc1
Le Quang Thang <lethang7794@gmail.com>
2025-02-21 16:46:50 +0700

    chore: config partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { loadEnv } from "vite";
-
+import partytown from "@astrojs/partytown";
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon()],
-
-	// Deployments
-	site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
-	base: env.PUBLIC_BASE_URL || "",
+  integrations: [tailwind(), icon(), partytown()],
+  // Deployments
+  site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
+  base: env.PUBLIC_BASE_URL || ""
 });
