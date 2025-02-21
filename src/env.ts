453d4aa95c3923dda837ea547af19a6b258755ff
Le Quang Thang <lethang7794@gmail.com>
2025-02-21 17:01:03 +0700

    feat: integrate partytown and Google Analytics

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,5 @@ export const env = {
 	PUBLIC_DOMAIN: import.meta.env.PUBLIC_DOMAIN || "",
 	PUBLIC_BASE_PATH: import.meta.env.PUBLIC_BASE_PATH || "",
 	PUBLIC_TIME: import.meta.env.PUBLIC_TIME || "",
+	PUBLIC_GA_ID: import.meta.env.PUBLIC_GA_ID || "",
 };
