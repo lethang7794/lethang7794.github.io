c6839505aab4b4045660f111832a4a9d029904af
Le Quang Thang <lethang7794@gmail.com>
2025-02-23 00:31:42 +0700

    fix: partytown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,18 @@ import { loadEnv } from "vite";
 import partytown from "@astrojs/partytown";
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), partytown()],
-  // Deployments
-  site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
-  base: env.PUBLIC_BASE_URL || ""
+	integrations: [
+		tailwind(),
+		icon(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
+	// Deployments
+	site: env.PUBLIC_DOMAIN ? `https://${env.PUBLIC_DOMAIN}` : undefined,
+	base: env.PUBLIC_BASE_URL || "",
 });
